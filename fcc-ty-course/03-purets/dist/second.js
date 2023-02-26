4e12ffa8e6c972871eeb972305ab0c2756fbb728
Finn <contact@bechinka.eu>
2023-02-26 15:58:29 +0100

    feat: 3:25:05

---
+++ fcc-ty-course/03-purets/dist/second.js
@@ -0,0 +1,11 @@
+"use strict";
+class Insta {
+    constructor(mode, filter, burst) {
+        this.mode = mode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    create_story() {
+        console.log("");
+    }
+}
